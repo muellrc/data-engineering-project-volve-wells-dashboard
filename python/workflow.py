90d779d483b6fb3dee1cbbed97e2c86bb277fbf8
Christian Mueller <muellrc@icloud.com>
2022-04-15 23:31:18 +0200

    Trying and trying
    Trying to learn how Luigi deals with input files from different Tasks

--- python/workflow.py
+++ python/workflow.py
@@ -1,12 +1,13 @@
 from logging import root
 import pandas as pd
 import luigi
+from luigi.util import requires, inherits, common_params
 from sqlalchemy import create_engine
 import luigi.contrib.postgres
 from lxml.etree import Element, XMLParser, parse
 from typing import Any
 from urllib.request import urlopen
-import requests
+import csv
 
 # ========================================EXTRACT========================================
 
@@ -14,7 +15,8 @@ import requests
 class extract_data(luigi.Task):
     def run(self):
 
-        filenames = ['app/source-files/VolveF.edm.1.xml', 'app/source-files/VolveF.edm.2.xml', 'app/source-files/VolveF.edm.3.xml']
+        filenames = ['app/source-files/VolveF.edm.1.xml', 'app/source-files/VolveF.edm.2.xml', 'app/source-files/VolveF.edm.3.xml', 
+        'app/source-files/VolveF.edm.4.xml', 'app/source-files/VolveF.edm.5.xml', 'app/source-files/VolveF.edm.6.xml']
         with self.output().open(mode="w") as f:
             for fname in filenames:
                  with open(fname) as infile:
@@ -29,21 +31,15 @@ class extract_data(luigi.Task):
 
 # ========================================TRANSFORM========================================
 
-# Step 1: Wells
-class transform_data_wellbores(luigi.Task):
 
-    def requires(self):
-        return [extract_data()]
-
-    def output(self):
-        return luigi.LocalTarget('wellbores.csv')
+@requires(extract_data)
+class TransformTask(luigi.Task):
+    """Step 1: Wells"""
 
     def run(self):
-
-        # parse EDM xml file
-        p = XMLParser(huge_tree=True)
-        with self.input().open() as infile:
-            tree = parse(infile, parser=p)
+        with self.input().open() as f:
+            p = XMLParser(huge_tree=True)
+            tree = parse(f, parser=p)
             root = tree.getroot()
 
         # create list to hold Wellbores
@@ -71,22 +67,69 @@ class transform_data_wellbores(luigi.Task):
 
         wellbores = pd.DataFrame(
             list(id_name_dict.items()), columns=['key', 'name'])
-        wellbores.to_csv('wellbores.csv', sep=';', index=False)
 
-
-# Step 2: Trajectories
-
-
-class transform_data_trajectories(luigi.Task):
-
-    def requires(self):
-        return [extract_data()]
+        with self.output().open(mode="w") as f:
+            writer = csv.writer(f, delimiter="\t")
+            writer.writerows(wellbores)
 
     def output(self):
-        return luigi.LocalTarget('trajectories.csv')
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(extract_data)
+class transform_data_wellbores(luigi.Task):
+    """Step 1: Wells"""
 
     def run(self):
-        # create a function to extract trajectory data from the database
+        with self.input().open() as f:
+            p = XMLParser(huge_tree=True)
+            tree = parse(f, parser=p)
+            root = tree.getroot()
+
+        # create list to hold Wellbores
+        wellbore_id = []
+
+        # get actual surveys for valid wellbores
+        for child in root:
+            if child.tag == 'CD_DEFINITIVE_SURVEY_HEADER':
+                if child.attrib['phase'] == 'ACTUAL':
+                    wellbore_id.append(child.attrib['wellbore_id'])
+
+        # create a list of wellbore names for the ids found
+        wellbore_name = []
+
+        for child in root:
+            if child.tag == 'CD_WELLBORE':
+                if child.attrib['wellbore_id'] in wellbore_id:
+                    wellbore_name.append(
+                        child.attrib['well_legal_name'].replace('/', '-'))
+
+        # make a dictionary holding wellbore ids as keys and wellbore names as values
+        id_name_dict = dict(zip(wellbore_id, wellbore_name))
+
+        id_name_dict
+
+        wellbores = pd.DataFrame(
+            list(id_name_dict.items()), columns=['key', 'name'])
+
+        with self.output().open(mode="w") as f:
+            writer = csv.writer(f, delimiter="\t")
+            writer.writerows(wellbores)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+
+@requires(extract_data)
+class transform_data_trajectories(luigi.Task):
+    """Step 2: Trajectories"""
+
+    def run(self):
+        with self.input().open() as f:
+            p = XMLParser(huge_tree=True)
+            tree = parse(f, parser=p)
+            root = tree.getroot()
 
         def get_wellpath(dct):
             survey_header = []
@@ -133,42 +176,47 @@ class transform_data_trajectories(luigi.Task):
                                 wellpath.to_csv(
                                     'trajectories.csv', sep=';', index=False)
 
+                with self.output().open(mode="w") as f:
+                    writer = csv.writer(f, delimiter="\t")
+                    writer.writerows(wellpath)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
 
 # =======================================================================================
 
 # ========================================LOAD========================================
 # Step 1: Wells
-
+@requires(transform_data_wellbores)
 class load_data_wellbores(luigi.Task):
-    def requires(self):
-        return [transform_data_wellbores()]
 
     def run(self):
-        data = pd.read_csv('wellbores.csv')
-        engine = create_engine(
-            'postgresql://postgres:postgres@dev-postgres-db:5432/wellbores_clean')
+        with self.input().open() as f:
+            data = pd.read_csv(f)
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/wellbores_clean')
 
-        data.to_sql(
-            'wellbores_clean',
-            engine,
-            index=False,
-            if_exists='replace'
-        )
+            data.to_sql(
+                'wellbores_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
 
     def output(self):
         return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'wellbores_clean', '1'))
 
 # Step 2: Trajectories
 
-
+@requires(transform_data_trajectories)
 class load_data_trajectories(luigi.Task):
-    def requires(self):
-        return [transform_data_trajectories()]
 
     def run(self):
-        data = pd.read_csv('trajectories.csv')
-        engine = create_engine(
-            'postgresql://postgres:postgres@dev-postgres-db:5432/trajectories_clean')
+        with self.input().open() as f:
+            data = pd.read_csv(f)
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/trajectories_clean')
 
         data.to_sql(
             'trajectories_clean',
