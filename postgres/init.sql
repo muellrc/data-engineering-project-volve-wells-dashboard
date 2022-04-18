c39ac4742e18303779f171f5e2ea5e7f191eeec0
Christian Mueller <muellrc@icloud.com>
2022-04-18 18:11:44 +0200

    Writing Well Technical Data and Production Data

--- postgres/init.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE wellbores_clean (wellbore_key varchar(15), wellbore_name varchar(50));
--- python/workflow.py
+++ python/workflow.py
@@ -1,17 +1,13 @@
-from fileinput import filename
 from logging import root
 import pandas as pd
 import luigi
-from luigi.util import requires, inherits, common_params
-from luigi.contrib import postgres
+from luigi.util import requires
 from sqlalchemy import create_engine
 import luigi.contrib.postgres
 from lxml.etree import Element, XMLParser, parse
 from typing import Any
 from urllib.request import urlopen
 import csv
-import psycopg2
-import os
 
 #Generic function to get all attributes for a specific EDM entity
 
@@ -28,7 +24,61 @@ def get_all_attributes(filename, tag):
 
     return result_values, keys
 
-class extract_data(luigi.Task):
+class extract_production_data(luigi.Task):
+    def run(self):
+        # Read source data (EDM Database Export) as one XML file instead of the intermediate files
+        filename = 'app/source-files/Volve production data.csv'
+        with self.output().open(mode="w") as f:
+            with open(filename) as infile:
+                for line in infile:
+                    f.write(line)
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(extract_production_data)
+class transform_production_data(luigi.Task):
+    def run(self):
+
+        with self.input().open() as f:
+            data = pd.read_csv(f, delimiter=";") 
+            data['NPD_WELL_BORE_NAME'] = 'NO ' + data['NPD_WELL_BORE_NAME'].astype(str)
+            list_data = data.values.tolist()
+            list_header = data.columns.to_list()
+
+        with self.output().open(mode="w") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(list_header)
+            writer.writerows(list_data[1:])
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+            
+
+@requires(transform_production_data)
+class load_data_production(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f, delimiter=";") 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'production_data',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'production_data', '1'))
+
+
+class extract_edm_data(luigi.Task):
     def run(self):
         # Read source data (EDM Database Export) as one XML file instead of the intermediate files
         filenames = ['app/source-files/VolveF.edm.1.xml', 'app/source-files/VolveF.edm.2.xml', 'app/source-files/VolveF.edm.3.xml', 
@@ -41,25 +91,26 @@ class extract_data(luigi.Task):
     def output(self):
         return luigi.LocalTarget(self.task_id + ".xml")
 
-# ========================================WELLS========================================
 
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_wells(luigi.Task):
     def run(self):
         # parse EDM XML file
         with self.input().open() as f: 
-
             result_values, keys = get_all_attributes(f, ".//CD_WELL")
+            
+            # remove invalid wells that have no Legal Name registered
+            result_filtered = [item for item in result_values if 'well_legal_name' in item]
+
 
         with self.output().open(mode="w") as f:
             # WRITE TO CSV VIA DICTWRITER
             dw = csv.DictWriter(f, fieldnames=keys)
             dw.writeheader()
-            dw.writerows(result_values)
+            dw.writerows(result_filtered)
 
     def output(self):
         return luigi.LocalTarget(self.task_id + ".csv")
-
 
 @requires(transform_data_wells)
 class load_data_wells(luigi.Task):
@@ -74,61 +125,35 @@ class load_data_wells(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'wells_clean',
+                'wells',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'wells_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'wells', '1'))
 
-# ==================================END OF WELLS==========================================
 
-        
-# ========================================WELLBORES========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_wellbores(luigi.Task):
     def run(self):
         # parse EDM XML file
-        with self.input().open() as f:
-            p = XMLParser(huge_tree=True)
-            tree = parse(f, parser=p)
-            root = tree.getroot()
+        with self.input().open() as f: 
+            result_values, keys = get_all_attributes(f, ".//CD_WELLBORE")
+            
+            # remove invalid wells that have no Legal Name registered
+            result_filtered = [item for item in result_values if 'well_legal_name' in item]
 
-        # create list to hold Wellbores
-        wellbore_id = []
-
-        # get only actual surveys for valid wellbores
-        for child_node in root:
-            if child_node.tag == 'CD_DEFINITIVE_SURVEY_HEADER':
-                if child_node.attrib['phase'] == 'ACTUAL':
-                    wellbore_id.append(child_node.attrib['wellbore_id'])
-
-        # create a list of wellbore legal names for the ids identified based on CD_WELLBORE table in EDM
-        wellbore_name = []
-        for child_node in root:
-            if child_node.tag == 'CD_WELLBORE':
-                if child_node.attrib['wellbore_id'] in wellbore_id:
-                    wellbore_name.append(
-                        child_node.attrib['well_legal_name'].replace('/', '-'))
-
-        # make a dictionary of wellbore ids and names
-        id_name_dict = dict(zip(wellbore_id, wellbore_name))
-
-        # write data to luigi output
-        wellbores = pd.DataFrame(
-            list(id_name_dict.items()), columns=['wellbore_key', 'wellbore_name'])
-        list_wellbores = wellbores.values.tolist()
 
         with self.output().open(mode="w") as f:
-            writer = csv.writer(f, delimiter="\t")
-            writer.writerows(list_wellbores[1:])
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_filtered)
 
     def output(self):
-        return luigi.LocalTarget(self.task_id + ".tsv")
-
+        return luigi.LocalTarget(self.task_id + ".csv")
 
 @requires(transform_data_wellbores)
 class load_data_wellbores(luigi.Task):
@@ -136,29 +161,24 @@ class load_data_wellbores(luigi.Task):
     def run(self):
 
         with self.input().open() as f:
-            # read tsv file holding valid Wellbores
-            reader = csv.reader(f,delimiter="\t")
-            # create dataframe
-            wellbores = pd.DataFrame(
-            reader, columns=['wellbore_key', 'wellbore_name'])
+            
+            data = pd.read_csv(f) 
+
             # write to postgres
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
-            wellbores.to_sql(
-                'wellbores_clean',
+            data.to_sql(
+                'wellbores',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'wellbores_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'wellbores', '1'))
 
-# ==================================END OF WELLBORES==========================================
 
-# ========================================DATUM========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_datum(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -189,20 +209,17 @@ class load_data_datum(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'datum_clean',
+                'datum',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'datum_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'datum', '1'))
 
-# ==================================END OF DATUM==========================================
 
-# ========================================SCENARIO========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_scenario(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -233,20 +250,17 @@ class load_data_scenario(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'scenario_clean',
+                'scenario',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'scenario_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'scenario', '1'))
 
-# ==================================END OF SCENARIO==========================================
 
-# ========================================DEFINITIVE SURVEY HEADER========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_definitive_survey_header(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -277,20 +291,17 @@ class load_data_definitive_survey_header(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'definitive_survey_header_clean',
+                'definitive_survey_header',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'definitive_survey_header_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'definitive_survey_header', '1'))
 
-# ==================================END OF DEFINITIVE SURVEY HEADER==========================================
 
-# ========================================DEFINITIVE SURVEY STATION========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_definitive_survey_station(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -321,20 +332,17 @@ class load_data_definitive_survey_station(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'definitive_survey_station_clean',
+                'definitive_survey_station',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'definitive_survey_station_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'definitive_survey_station', '1'))
 
-# ==================================END OF DEFINITIVE SURVEY STATION==========================================
 
-# ========================================SURVEY HEADER========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_survey_header(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -365,20 +373,17 @@ class load_data_survey_header(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'survey_header_clean',
+                'survey_header',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'survey_header_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'survey_header', '1'))
 
-# ==================================END OF SURVEY HEADER==========================================
 
-# ========================================SURVEY STATION========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_survey_station(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -409,20 +414,17 @@ class load_data_survey_station(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'survey_station_clean',
+                'survey_station',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'survey_station_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'survey_station', '1'))
 
-# ==================================END OF SURVEY STATION==========================================
 
-# ========================================ASSEMBLY========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_assembly(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -453,20 +455,17 @@ class load_data_assembly(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'assembly_clean',
+                'assembly',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'assembly_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'assembly', '1'))
 
-# ==================================END OF ASSEMBLY==========================================
 
-# ========================================POLICY========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_policy(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -497,20 +496,17 @@ class load_data_policy(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'policy_clean',
+                'policy',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'policy_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'policy', '1'))
 
-# ==================================END OF POLICY==========================================
 
-# ========================================PROJECT========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_project(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -541,20 +537,17 @@ class load_data_project(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'project_clean',
+                'project',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'project_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'project', '1'))
 
-# ==================================END OF PROJECT==========================================
 
-# ========================================SITE========================================
-
-@requires(extract_data)
+@requires(extract_edm_data)
 class transform_data_site(luigi.Task):
     def run(self):
         # parse EDM XML file
@@ -585,58 +578,58 @@ class load_data_site(luigi.Task):
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
             data.to_sql(
-                'site_clean',
+                'site',
                 engine,
                 index=False,
                 if_exists='replace'
             )
 
     def output(self):
-        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'site_clean', '1'))
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'site', '1'))
 
-# ==================================END OF SITE==========================================
-
-# ========================================RUN PIPELINE========================================
 
 class workflow(luigi.Task):
     def run(self):
     #RunPipelines
 
-        #1 - Wells
+        # Production Data
+        luigi.build([load_data_production()])
+
+        # Wells
         luigi.build([load_data_wells()])
 
-        #2 - Wellbores
+        # Wellbores
         luigi.build([load_data_wellbores()])
 
-        #3 - Datum
+        # Datum
         luigi.build([load_data_datum()])
 
-        #4 - Scenario
+        # Scenario
         luigi.build([load_data_scenario()])
 
-        #5 - Definitive survey header
+        # Definitive survey header
         luigi.build([load_data_definitive_survey_header()])
 
-        #6 - Definitive survey station
+        # Definitive survey station
         luigi.build([load_data_definitive_survey_station()])
 
-        #7 - Survey header
+        # Survey header
         luigi.build([load_data_survey_header()])
 
-        #8 - Survey station
+        # Survey station
         luigi.build([load_data_survey_station()])
 
-        #9 - Assembly
+        # Assembly
         luigi.build([load_data_assembly()])
 
-        #10 - Policy
+        # Policy
         luigi.build([load_data_policy()])
 
-        #11 - Project
+        # Project
         luigi.build([load_data_project()])
 
-        #12 - Site
+        # Site
         luigi.build([load_data_site()])
 
 
-# =======================================================================================
+
