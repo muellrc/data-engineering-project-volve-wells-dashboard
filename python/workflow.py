da0f02a73bc8c76361fdf44d2d59d3773710f784
Christian Mueller <muellrc@icloud.com>
2022-04-18 10:59:57 +0200

    Writing to postgres
    Added selected EDM tables:
    
    - Wells
    - Wellbores
    - Datum
    - Scenario
    - Definitive Survey header
    - Definitive Survey station
    - Survey header
    - Survey station
    - Assembly
    - Policy
    - Project
    - Site

--- python/workflow.py
+++ python/workflow.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from logging import root
 import pandas as pd
 import luigi
@@ -12,6 +13,21 @@ import csv
 import psycopg2
 import os
 
+#Generic function to get all attributes for a specific EDM entity
+
+def get_all_attributes(filename, tag):
+    p = XMLParser(huge_tree=True) 
+    tree = parse(filename, parser=p) 
+    root = tree.getroot() 
+
+    # RETURN LIST OF ATTRIBUTE DICTIONARIES
+    result_values = [dict(n.attrib) for n in root.findall(tag)]
+
+    # RETRIEVE UNIQUE KEYS FOR COLUMN HEADERS
+    keys = list({k for dct in result_values for k in dct})
+
+    return result_values, keys
+
 class extract_data(luigi.Task):
     def run(self):
         # Read source data (EDM Database Export) as one XML file instead of the intermediate files
@@ -25,8 +41,6 @@ class extract_data(luigi.Task):
     def output(self):
         return luigi.LocalTarget(self.task_id + ".xml")
 
-
-      
 # ========================================WELLS========================================
 
 @requires(extract_data)
@@ -34,15 +48,8 @@ class transform_data_wells(luigi.Task):
     def run(self):
         # parse EDM XML file
         with self.input().open() as f: 
-            p = XMLParser(huge_tree=True) 
-            tree = parse(f, parser=p) 
-            root = tree.getroot() 
 
-            # RETURN LIST OF ATTRIBUTE DICTIONARIES
-            result_values = [dict(n.attrib) for n in root.findall(".//CD_WELL")]
-
-            # RETRIEVE UNIQUE KEYS FOR COLUMN HEADERS
-            keys = list({k for dct in result_values for k in dct})
+            result_values, keys = get_all_attributes(f, ".//CD_WELL")
 
         with self.output().open(mode="w") as f:
             # WRITE TO CSV VIA DICTWRITER
@@ -61,12 +68,12 @@ class load_data_wells(luigi.Task):
 
         with self.input().open() as f:
             
-            wells = pd.read_csv(f) 
+            data = pd.read_csv(f) 
 
             # write to postgres
             engine = create_engine(
                 'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
-            wells.to_sql(
+            data.to_sql(
                 'wells_clean',
                 engine,
                 index=False,
@@ -76,7 +83,7 @@ class load_data_wells(luigi.Task):
     def output(self):
         return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'wells_clean', '1'))
 
-# ==================================END OF WELLBORES==========================================
+# ==================================END OF WELLS==========================================
 
         
 # ========================================WELLBORES========================================
@@ -149,8 +156,445 @@ class load_data_wellbores(luigi.Task):
 
 # ==================================END OF WELLBORES==========================================
 
+# ========================================DATUM========================================
+
+@requires(extract_data)
+class transform_data_datum(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_DATUM")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
 
 
+@requires(transform_data_datum)
+class load_data_datum(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'datum_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'datum_clean', '1'))
+
+# ==================================END OF DATUM==========================================
+
+# ========================================SCENARIO========================================
+
+@requires(extract_data)
+class transform_data_scenario(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_SCENARIO")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_scenario)
+class load_data_scenario(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'scenario_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'scenario_clean', '1'))
+
+# ==================================END OF SCENARIO==========================================
+
+# ========================================DEFINITIVE SURVEY HEADER========================================
+
+@requires(extract_data)
+class transform_data_definitive_survey_header(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_DEFINITIVE_SURVEY_HEADER")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_definitive_survey_header)
+class load_data_definitive_survey_header(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'definitive_survey_header_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'definitive_survey_header_clean', '1'))
+
+# ==================================END OF DEFINITIVE SURVEY HEADER==========================================
+
+# ========================================DEFINITIVE SURVEY STATION========================================
+
+@requires(extract_data)
+class transform_data_definitive_survey_station(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_DEFINITIVE_SURVEY_STATION")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_definitive_survey_station)
+class load_data_definitive_survey_station(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'definitive_survey_station_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'definitive_survey_station_clean', '1'))
+
+# ==================================END OF DEFINITIVE SURVEY STATION==========================================
+
+# ========================================SURVEY HEADER========================================
+
+@requires(extract_data)
+class transform_data_survey_header(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_SURVEY_HEADER")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_survey_header)
+class load_data_survey_header(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'survey_header_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'survey_header_clean', '1'))
+
+# ==================================END OF SURVEY HEADER==========================================
+
+# ========================================SURVEY STATION========================================
+
+@requires(extract_data)
+class transform_data_survey_station(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_SURVEY_STATION")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_survey_station)
+class load_data_survey_station(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'survey_station_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'survey_station_clean', '1'))
+
+# ==================================END OF SURVEY STATION==========================================
+
+# ========================================ASSEMBLY========================================
+
+@requires(extract_data)
+class transform_data_assembly(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_ASSEMBLY")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_assembly)
+class load_data_assembly(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'assembly_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'assembly_clean', '1'))
+
+# ==================================END OF ASSEMBLY==========================================
+
+# ========================================POLICY========================================
+
+@requires(extract_data)
+class transform_data_policy(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_POLICY")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_policy)
+class load_data_policy(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'policy_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'policy_clean', '1'))
+
+# ==================================END OF POLICY==========================================
+
+# ========================================PROJECT========================================
+
+@requires(extract_data)
+class transform_data_project(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_PROJECT")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_project)
+class load_data_project(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'project_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'project_clean', '1'))
+
+# ==================================END OF PROJECT==========================================
+
+# ========================================SITE========================================
+
+@requires(extract_data)
+class transform_data_site(luigi.Task):
+    def run(self):
+        # parse EDM XML file
+        with self.input().open() as f: 
+
+            result_values, keys = get_all_attributes(f, ".//CD_SITE")
+
+        with self.output().open(mode="w") as f:
+            # WRITE TO CSV VIA DICTWRITER
+            dw = csv.DictWriter(f, fieldnames=keys)
+            dw.writeheader()
+            dw.writerows(result_values)
+
+    def output(self):
+        return luigi.LocalTarget(self.task_id + ".csv")
+
+
+@requires(transform_data_site)
+class load_data_site(luigi.Task):
+
+    def run(self):
+
+        with self.input().open() as f:
+            
+            data = pd.read_csv(f) 
+
+            # write to postgres
+            engine = create_engine(
+                'postgresql://postgres:postgres@dev-postgres-db:5432/postgres')
+            data.to_sql(
+                'site_clean',
+                engine,
+                index=False,
+                if_exists='replace'
+            )
+
+    def output(self):
+        return(luigi.contrib.postgres.PostgresTarget('dev-postgres-db', 'postgres', 'postgres', 'postgres', 'site_clean', '1'))
+
+# ==================================END OF SITE==========================================
 
 # ========================================RUN PIPELINE========================================
 
@@ -158,10 +602,41 @@ class workflow(luigi.Task):
     def run(self):
     #RunPipelines
 
-        #1 - Wellbores
-        #luigi.build([load_data_wellbores()])
-
-        #2 - Trajectories
+        #1 - Wells
         luigi.build([load_data_wells()])
+
+        #2 - Wellbores
+        luigi.build([load_data_wellbores()])
+
+        #3 - Datum
+        luigi.build([load_data_datum()])
+
+        #4 - Scenario
+        luigi.build([load_data_scenario()])
+
+        #5 - Definitive survey header
+        luigi.build([load_data_definitive_survey_header()])
+
+        #6 - Definitive survey station
+        luigi.build([load_data_definitive_survey_station()])
+
+        #7 - Survey header
+        luigi.build([load_data_survey_header()])
+
+        #8 - Survey station
+        luigi.build([load_data_survey_station()])
+
+        #9 - Assembly
+        luigi.build([load_data_assembly()])
+
+        #10 - Policy
+        luigi.build([load_data_policy()])
+
+        #11 - Project
+        luigi.build([load_data_project()])
+
+        #12 - Site
+        luigi.build([load_data_site()])
+
 
 # =======================================================================================
